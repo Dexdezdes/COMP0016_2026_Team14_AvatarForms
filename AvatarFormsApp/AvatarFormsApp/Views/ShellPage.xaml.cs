a2ee1665e24b1bc45bcc91a942c151e29d4dfdcb
Fakhrur219 <alrazi2003@gmail.com>
2026-03-24 16:52:53 +0000

    Added settings in shell page

--- AvatarFormsApp/AvatarFormsApp/Views/ShellPage.xaml.cs
+++ AvatarFormsApp/AvatarFormsApp/Views/ShellPage.xaml.cs
@@ -10,11 +10,15 @@ using Microsoft.UI.Xaml.Media;
 
 using Windows.System;
 using System.Collections.Specialized;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Media.SpeechRecognition;
 
 namespace AvatarFormsApp.Views;
 
 public sealed partial class ShellPage : Page
 {
+    private readonly ILocalSettingsService _localSettingsService;
     public Frame NavigationFrameControl => NavigationFrame;
     public ShellPageViewModel ViewModel
     {
@@ -26,6 +30,8 @@ public sealed partial class ShellPage : Page
         ViewModel = viewModel;
         InitializeComponent();
 
+        _localSettingsService = App.GetService<ILocalSettingsService>();
+
         ViewModel.NavigationService.Frame = NavigationFrame;
         ViewModel.NavigationViewService.Initialize(NavigationViewControl);
 
@@ -34,6 +40,15 @@ public sealed partial class ShellPage : Page
 
         // Initialize the questionnaire menu items
         UpdateQuestionnaireMenuItems();
+
+        ViewModel.NavigationService.Navigated += ShellNavigationService_Navigated;
+
+        _ = InitializeSettingsAsync();
+    }
+
+    private void ShellNavigationService_Navigated(object sender, Microsoft.UI.Xaml.Navigation.NavigationEventArgs e)
+    {
+        _ = InitializeSettingsAsync(); // Refresh settings state just in case it was changed
     }
 
     private void OnLoaded(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
@@ -73,6 +88,17 @@ public sealed partial class ShellPage : Page
 
     private void OnItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
     {
+        if (args.IsSettingsInvoked)
+        {
+            if (ShellSettingsOverlay != null)
+            {
+                ShellSettingsOverlay.Visibility = ShellSettingsOverlay.Visibility == Visibility.Visible 
+                    ? Visibility.Collapsed 
+                    : Visibility.Visible;
+            }
+            return;
+        }
+
         if (args.InvokedItemContainer is NavigationViewItem item)
         {
             // Sub-items under Responses have a questionnaire ID as Tag (not a page key)
@@ -107,6 +133,116 @@ public sealed partial class ShellPage : Page
             };
 
             Nav_Responses.MenuItems.Add(menuItem);
+        }
+    }
+    private async Task InitializeSettingsAsync()
+    {
+        var languages = SpeechRecognizer.SupportedTopicLanguages;
+        if (LanguageComboBox != null)
+        {
+            LanguageComboBox.ItemsSource = languages;
+
+            var savedLang = await _localSettingsService.ReadSettingAsync<string>("SelectedSpeechLanguage");
+            if (!string.IsNullOrEmpty(savedLang))
+            {
+                var match = languages.FirstOrDefault(l => l.LanguageTag == savedLang);
+                if (match != null)
+                {
+                    LanguageComboBox.SelectedItem = match;
+                }
+            }
+
+            if (LanguageComboBox.SelectedItem == null && languages.Count > 0)
+            {
+                var defaultLang = languages.FirstOrDefault(l => l.LanguageTag == SpeechRecognizer.SystemSpeechLanguage.LanguageTag) ?? languages[0];
+                LanguageComboBox.SelectedItem = defaultLang;
+            }
+        }
+
+        var savedAvatar = await _localSettingsService.ReadSettingAsync<string>("SelectedAvatar");
+        if (!string.IsNullOrEmpty(savedAvatar))
+        {
+            if (AvatarComboBox != null)
+            {
+                foreach (ComboBoxItem item in AvatarComboBox.Items)
+                {
+                    if (item.Tag is string tag && tag == savedAvatar)
+                    {
+                        AvatarComboBox.SelectedItem = item;
+                        break;
+                    }
+                }
+            }
+        }
+        else if (AvatarComboBox != null)
+        {
+            AvatarComboBox.SelectedIndex = 0;
+        }
+
+        var savedVoice = await _localSettingsService.ReadSettingAsync<string>("SelectedVoice");
+        if (!string.IsNullOrEmpty(savedVoice))
+        {
+            if (VoiceComboBox != null)
+            {
+                foreach (ComboBoxItem item in VoiceComboBox.Items)
+                {
+                    if (item.Tag is string tag && tag == savedVoice)
+                    {
+                        VoiceComboBox.SelectedItem = item;
+                        break;
+                    }
+                }
+            }
+        }
+        else if (VoiceComboBox != null)
+        {
+            VoiceComboBox.SelectedIndex = 0;
+        }
+
+        var savedAutoSend = await _localSettingsService.ReadSettingAsync<bool?>("AutoSendEnabled");
+        if (savedAutoSend != null && AutoSendToggle != null)
+        {
+            AutoSendToggle.IsOn = savedAutoSend.Value;
+        }
+    }
+
+    private void OnCloseShellSettingsClicked(object sender, RoutedEventArgs e)
+    {
+        if (ShellSettingsOverlay != null)
+        {
+            ShellSettingsOverlay.Visibility = Visibility.Collapsed;
+        }
+    }
+
+    private void OnAutoSendToggled(object sender, RoutedEventArgs e)
+    {
+        if (AutoSendToggle != null)
+        {
+            _ = _localSettingsService?.SaveSettingAsync("AutoSendEnabled", AutoSendToggle.IsOn);
+        }
+    }
+
+    private void OnAvatarSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (sender is ComboBox cb && cb.SelectedItem is ComboBoxItem item && item.Tag is string tag)
+        {
+            _ = _localSettingsService?.SaveSettingAsync("SelectedAvatar", tag);
+        }
+    }
+
+    private void OnVoiceSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (sender is ComboBox cb && cb.SelectedItem is ComboBoxItem item && item.Tag is string tag)
+        {
+            _ = _localSettingsService?.SaveSettingAsync("SelectedVoice", tag);
+        }
+    }
+
+    private void OnLanguageSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (sender is ComboBox cb && cb.SelectedItem is Windows.Globalization.Language lang)
+        {
+            _ = _localSettingsService?.SaveSettingAsync("SelectedSpeechLanguage", lang.LanguageTag);
         }
     }
 }
